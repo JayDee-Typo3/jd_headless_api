6f5584d5d20d2f56a343797741155a969b90a08c
Delesky Johannes <delesky@me.com>
2025-06-24 23:36:14 +0200

    [FEAT]: configure api plugin

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,4 +1,18 @@
 <?php
+
+use JD\JdHeadlessApi\Controller\ApiController;
+use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
+
 defined('TYPO3') or die('Access denied');
 
-(function () {})();
+(function () {
+    ExtensionUtility::configurePlugin(
+        'JdHeadlessApi',
+        'Api',
+        [
+            ApiController::class => 'index'
+        ],
+        [],
+        ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT
+    );
+})();
